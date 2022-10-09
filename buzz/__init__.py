a96fdc6f3d23e4973e678440fecfa19dbffc3cb1
mp4 <90736838+mp4-dev@users.noreply.github.com>
2022-10-09 15:36:42 -0400

    added base database functionality

--- buzz/__init__.py
+++ buzz/__init__.py
@@ -5,15 +5,23 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY='dev',
+        DATABASE='database.db'
     )
 
     if test_config != None:
         app.config.from_mapping(test_config)
     else:
         app.config.from_pyfile('config.py')
-    
+
+    from . import db
+    db.init_app(app)
+
     @app.route("/hello")
     def hello():
         return "hello world"
     
+    @app.route("/")
+    def index():
+        return
+
     return app
---
+++ buzz/db.py
@@ -0,0 +1,40 @@
+import sqlite3
+import click
+from flask import current_app, g
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(current_app.config['DATABASE'])
+    db.row_factory = make_dicts
+    return db
+
+
+def close_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value) for idx, value in enumerate(row))
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def init_db(e=None):
+    with current_app.app_context():
+        db = get_db()
+        with current_app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@click.command('init-db')
+def initialize_database():
+    init_db()
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(initialize_database)
